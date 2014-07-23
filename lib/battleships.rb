02e8e113ca394cafc4ec8850e3f902b3ade57c41
BobRazoswki <bobongithub@gmail.com>
2014-07-23 15:24:22 +0100

    adding session, 4k issue

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,7 +5,7 @@ require_relative '../files'
 class BattleShipsGroup < Sinatra::Base
 
 	set :views, Proc.new { File.join(root, "..", 'views') }
-
+  set :session_secret, "bob"
   enable :sessions
 
   get '/' do
@@ -18,13 +18,11 @@ class BattleShipsGroup < Sinatra::Base
 
  post '/getreadytoplay' do
     name1 = params[:player1]
-    @player1 = Player.new(name: name1, board: Board.new(content: Water.new))
-    name2 = params[:player2]
-    @player2 = Player.new(name: name2, board: Board.new(content: Water.new))
-  	@game = Game.new(player_one: @player1, player_two: @player2)
-    # puts @game.inspect
+    session[:player1] = Player.new(name: name1, board: Board.new(content: Water.new))
+   name2 = params[:player2]
+   session[:player2] = Player.new(name: name2, board: Board.new(content: Water.new))
+  	session[:game]= Game.new(player_one: session[:player1], player_two: session[:player2])
     erb :getreadytoplay
-
   end
 
 post '/newgame' do  # POST /newgame HTTP/1.1
